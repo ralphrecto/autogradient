9d6004cd83d00ca3a0360ba0650dd0a2eced5316
Ralph Recto <recto.ralph@gmail.com>
2018-02-19 13:35:12 -0500

    Finished computation DAG edge generation from named expression trees.

--- expr.ml
+++ expr.ml
@@ -45,6 +45,23 @@ let name_tree (tree: 'a expr) : string expr =
 
   name_subtree String.Map.empty tree |> snd
 
+(* Retrieves name/string tag of a given expr node. *)
+let get_name = function
+  | Const (name, _) | Var (name, _) | Binop (name, _, _, _) -> name
+
+(* Generates the edges of the computation DAG given a named tree of expressions. A node wi depends on a
+ * node wj if wj is used in the definition of wi. Such a dependency induces an edge of form (wj, wi) to be
+ * included in the output. In tree terms, each parent depends on each of its children. *)
+let generate_dag_edges (named_tree: string expr) : (string * string) list =
+  let rec inner (edges: (string * string) list) (named_subtree: string expr) : (string * string) list =
+    match named_subtree with
+    | Const _ | Var _ -> edges
+    | Binop (name, _, left_subtree, right_subtree) ->
+        let subtree_edges = inner (inner edges left_subtree) right_subtree in
+        (get_name left_subtree, name) :: (get_name right_subtree, name) :: subtree_edges in
+
+  inner [] named_tree
+
 let () =
   let sigmoid: unit expr =
     Binop ((),
